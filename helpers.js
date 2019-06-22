4c8a36ffaa90784b4e305110be4670757ce0500b
Giuseppe Burtini <giuseppe@burtini.ca>
2019-06-22 08:20:23 -0700

    ...

--- helpers.js
+++ helpers.js
@@ -1,4 +1,4 @@
-module.exports = (settings, context) = {
+module.exports = (settings, context) => ({
   masqueradeEndpoint: async function(req, res, next) {
     const id = req.params.id;
     if (!id) {
@@ -7,24 +7,25 @@ module.exports = (settings, context) = {
       return res.json(settings.successResponse);
     }
 
-    if (!settings.canMasquerade(context.getRealUser(req.user), id)) { 
-        return res.json(settings.failureResponse);
+    if (!settings.canMasquerade(context.getRealUser(req.user), id)) {
+      return res.json(settings.failureResponse);
     }
 
     context.setMasquerading(req, id);
     return res.json(settings.successResponse);
   },
 
-
   serializeUser: (potentiallyMasqueradedUser, done) => {
-      const user = context.getRealUser(potentiallyMasqueradedUser);
+    const user = context.getRealUser(potentiallyMasqueradedUser);
     done(null, user.id);
   },
 
   deserializeUser: async (id, done) => {
     try {
-        const user = await settings.getUserById(id)
-        done(null, user);
-    } catch (e) { done(e); }        
+      const user = await settings.getUserById(id);
+      done(null, user);
+    } catch (e) {
+      done(e);
+    }
   }
-};
+});
